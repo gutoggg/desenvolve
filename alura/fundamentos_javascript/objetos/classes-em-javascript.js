1ab9a0149723c519bcc722ed2e98058747d450da
gutoggg <72248716+gutoggg@users.noreply.github.com>
2022-02-17 08:22:03 -0300

    adiciona o arquivo classes-em-javascript.js

--- alura/fundamentos_javascript/objetos/classes-em-javascript.js
+++ alura/fundamentos_javascript/objetos/classes-em-javascript.js
@@ -8,27 +8,28 @@ class contaPoupanca {
         this.nome = nome;
         this.cpf = cpf;
         this.saldo = saldo;
-        this.depositar = function(valor) {
-            this.saldo += valor
-        }
-        this.sacar = function(valor) {
-            if (valor > this.saldo) {
-                console.log(`Saque negado. Conta possui apenas R$${this.saldo} em saldo.`)
-            } else {
-                this.saldo -= valor
-                console.log(`Saque efetuado. Conta possui agora R$${this.saldo} em saldo.`)
-            }
-
-        }
-        
-
+    
         console.log(`Conta criada em nome de ${this.nome} com saldo inicial de R$${this.saldo}.`);
+    }
+
+    depositar(valor) {
+        this.saldo += valor
+    }
+
+    sacar(valor) {
+        if (valor > this.saldo) {
+            console.log(`Saque negado. Conta possui apenas R$${this.saldo} em saldo e foi solicitado o saque de R$${valor}.`)
+        } else {
+            this.saldo -= valor
+            console.log(`Saque efetuado. Conta possui agora R$${this.saldo} em saldo.`)
+        }
     }
 }
 
 // Extende a conta poupança para criação da conta com um dependente
 class contaComDependente extends contaPoupanca{
     constructor(nomeTitular, cpfTitular, nomeDependente, cpfDependente, saldo){
+        //por baixo dos panos, "copia" a base da classe a ser extendida para dentro da nova classe
         super(nomeTitular, cpfTitular, saldo);
         this.nomeDependente = nomeDependente;
         this.cpfDependente = cpfDependente
@@ -40,5 +41,6 @@ const contaPoup1 = new contaPoupanca("Jorge", 1234, 150);
 const conta2 = new contaPoupanca("Carlos", 32565, 1000000)
 const contaComJessica = new contaComDependente("Jorge", 1234, "Jessica", 4321, 250);
 
-// E agora utiliza o método herdado da contaPou
-contaComJessica.sacar(300);
+
+// E agora utiliza em uma contaComDependente o método herdado da classe contaPoup
+contaComJessica.sacar(250);
